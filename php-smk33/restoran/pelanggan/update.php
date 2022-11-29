b5827465ffd4af8b8a0f0a50d18b63621250ab73
Gestananda Faza <119272849+Faza2006@users.noreply.github.com>
2022-11-29 13:34:50 +0700

    PHP SMK 33

---
+++ php-smk33/restoran/pelanggan/update.php
@@ -0,0 +1,23 @@
+<?php 
+
+if (isset($_GET['id'])) {
+    $id=$_GET['id'];
+
+    $row = $db->getITEM("SELECT * FROM tblpelanggan WHERE idpelanggan = $id");
+
+    if ($row['aktif']==0) {
+        $aktif= 1;
+    }else {
+        $aktif = 0;
+    }
+
+    
+    $sql = "UPDATE tblpelanggan SET aktif=$aktif WHERE idpelanggan=$id";
+    echo $sql;
+    $db->runSQL($sql);
+
+    header("location:?f=pelanggan&m=select");
+}
+
+
+?>
